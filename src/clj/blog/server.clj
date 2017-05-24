31556828e19ca28bdce29aaa61252a954806e9ed
ogawa-rni <k.ogawa@r-n-i.jp>
2017-05-24 23:47:07 +0900

    fix db setting

--- src/clj/blog/server.clj
+++ src/clj/blog/server.clj
@@ -1,9 +1,19 @@
 (ns blog.server
   (:require [blog.handler :refer [handler]]
             [config.core :refer [env]]
-            [ring.adapter.jetty :refer [run-jetty]])
+            [ring.adapter.jetty :refer [run-jetty]]
+            [migratus.core :as migratus])
   (:gen-class))
 
- (defn -main [& args]
-   (let [port (Integer/parseInt (or (env :port) "3000"))]
-     (run-jetty handler {:port port :join? false})))
+(def config {:store            :database
+             :migration-dir    "migrations"
+             :db {:classname   "com.mysql.cj.jdbc.Driver"
+                  :subprotocol "mysql"
+                  :subname     "//localhost/blog"
+                  :user        "blog"
+                  :password    ""}})
+
+(defn -main [& args]
+  (migratus/migrate config)
+  (run-jetty handler {:port (Integer/parseInt (or (env :port) "3000"))
+                      :join? false}))
